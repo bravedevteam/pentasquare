ec0debe6ca207e8f28180883b05d03a3664a9edc
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2021-08-17 10:02:39 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(function(){
-	
+	$(".btnConnect").click(function(){
+    $(".boxWallet").fadeIn(500);
+  });
+
+  $(".btnMypage").click(function(){
+    $(".boxWallet").fadeIn(500);
+    $(".userLink .listDepth").slideUp(0);
+  });
+
+  $(".userLink .depth").click(function(){
+    $(this).toggleClass("on");
+    $(".userLink .listDepth").slideToggle(300);
+  });
+
+  $(".boxWallet").on("mouseleave", function(){
+    $(".boxWallet").fadeOut(500);
+  });
 });
